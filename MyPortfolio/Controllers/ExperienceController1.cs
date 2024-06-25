55123ff159db34b16373323c8f54ea02eff9ebdb
demir <demir@OMER-PC>
2024-06-25 14:58:11 +0300

    Add project files.

---
+++ MyPortfolio/Controllers/ExperienceController1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class ExperienceController1 : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult ExperienceList()
+        {
+            var values = context.Experinces.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experince experience)
+        {
+            context.Experinces.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = context.Experinces.Find(id);
+            context.Experinces.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = context.Experinces.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+
+        public IActionResult UpdateExperience(Experince experience)
+        {
+            context.Experinces.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList"); 
+        }
+    }
+}
---
+++ MyPortfolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.DAL.Context
+{
+    public class MyPortfolioContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=OMER-PC\\SQLEXPRESS;initial Catalog=MyPortfolioDb;integrated Security=true;Trust Server Certificate =true");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experince> Experinces{ get; set; }
+        public DbSet<Feature> Features{ get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios{ get; set; }
+        public DbSet<Skill> Skills{ get; set; }
+        public DbSet<SocialMedia> SocialMedias{ get; set; }
+        public DbSet<Testimonial> Testimonials{ get; set; }
+        public DbSet<ToDoList> ToDoLists{ get; set; }
+
+	}
+}
